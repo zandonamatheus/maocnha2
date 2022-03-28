1fd27a3dcc4e0e296de7596ca4e9d95f3f1b300d
zandonamatheus <82128531+zandonamatheus@users.noreply.github.com>
2022-03-28 11:21:54 -0300

    Add files via upload

---
+++ sitell/javas/javascript.js
@@ -0,0 +1,24 @@
+var valor1, bb;
+
+valor1=69;
+document.write("<div class=diferente> Matheus Zandoná ↑</div>");
+
+
+idade= new Array();
+idade[0]=15;
+idade[1]=24;
+idade[2]=38;
+document.write('<h3> número do sorteio:  '+idade[1]+' </h3>')
+
+alert('league of legends 2');
+
+valor1=prompt('mark zuckerberg');
+document.write('<h3> desejo '+valor1+' para você </h3>');
+
+bb=confirm('deseja sair do app?');
+document.write('<h3> você é homossexual? '+bb+' </h3> ');
+
+s1=prompt('digite um número:');
+s2=prompt('outro número');
+soma=parseInt(s1)+parseInt(s2);
+document.write('<h3> dei minha bunda '+soma+' vezes</h3>')
